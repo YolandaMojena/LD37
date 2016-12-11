2daeb344cf5c6ea13d2a62afa6ee1bdd79328ea2
EtherealWhisper <yomowilce@gmail.com>
2016-12-11 01:45:27 +0100

    Envelopes and hearts

---
+++ Assets/Scripts/Message.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Message : MonoBehaviour {
+
+    [SerializeField]
+    private SpriteRenderer smallIcon;
+    [SerializeField]
+    private SpriteRenderer largeIcon;
+    [SerializeField]
+    private MeshRenderer hairColor;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void SetToradoreo(Sprite icon, Color color)
+    {
+        smallIcon.sprite = icon;
+        largeIcon.sprite = icon;
+        hairColor.material.SetColor("_EmissionColor", color);
+    }
+}
