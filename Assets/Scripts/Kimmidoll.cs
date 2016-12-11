d6824dc85de790c52fca949f4b74b0936c698807
therecklesspulse <al264106@uji.es>
2016-12-11 16:27:58 +0100

    Cute Children! :squirrel:

--- Assets/Scripts/Kimmidoll.cs
+++ Assets/Scripts/Kimmidoll.cs
@@ -8,8 +8,17 @@ public class Kimmidoll : MonoBehaviour {
     [SerializeField]
     Transform RArm;
 
+    Quaternion LInitialRotation;
+    Quaternion RInitialRotation;
+    Quaternion initialRotation;
+
+    const float AMPLITUDE = 10f;
+
     bool excited = false;
     bool LArm_excited = true;
+    bool excitement_boost = false;
+    float timer = 0f;
+    const float BOOST_INTERVAL = 3f;
 
     //Kimmidoll -> 25
     //Arm       -> [-25, -40]
@@ -17,42 +26,95 @@ public class Kimmidoll : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
+        LInitialRotation = LArm.rotation;
+        RInitialRotation = RArm.rotation;
+        initialRotation = transform.rotation;
         BecomeExcited();
+        //StopExcitement();
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
         if (excited)
         {
-            /*if (LArm_excited)
+            // Initial Steep
+            if (LArm_excited)
             {
-
+                /*Vector3 targetRotation = new Vector3(0f,0f,335f);
+                transform.eulerAngles = new Vector3(Mathf.Lerp(transform.eulerAngles.x, targetRotation.x, Time.deltaTime),
+                                                    Mathf.Lerp(transform.eulerAngles.y, targetRotation.y, Time.deltaTime),
+                                                    Mathf.Lerp(transform.eulerAngles.z, targetRotation.z, Time.deltaTime));*/
+                Quaternion currentRotation = transform.rotation;
+                transform.eulerAngles = new Vector3(0f, 0f, 335f);
+                Quaternion targetRotation = transform.rotation;
+                transform.rotation = currentRotation;
+                transform.rotation = Quaternion.Lerp(transform.rotation,targetRotation, Time.deltaTime);
             }
             else
             {
+                Vector3 targetRotation = new Vector3(0f, 0f, 25f);
+                transform.eulerAngles = new Vector3(Mathf.Lerp(transform.eulerAngles.x, targetRotation.x, Time.deltaTime),
+                                                    Mathf.Lerp(transform.eulerAngles.y, targetRotation.y, Time.deltaTime),
+                                                    Mathf.Lerp(transform.eulerAngles.z, targetRotation.z, Time.deltaTime));
+            }
 
-            }*/
-            RArm.eulerAngles = new Vector3(-35f + Mathf.Sin(Time.time*5f) * 15f, 90, -90);
+
+            //Boost enabler
+            if (timer < BOOST_INTERVAL)
+                timer += Time.deltaTime;
+            else
+            {
+                if (excitement_boost)
+                {
+                    excitement_boost = false;
+                    timer = 0f;
+                }
+                else if (Random.value > 0.5f)
+                {
+                    excitement_boost = true;
+                    timer = BOOST_INTERVAL - 1f;
+                }
+                else
+                    timer = 0f;
+            }
+
+            // ARM WAVING
+            float freq_booster = 5f;
+            if (excitement_boost) freq_booster = 50f;
+
+            if (LArm_excited)
+            {
+                LArm.eulerAngles = new Vector3(-40f + Mathf.Sin(Time.time * freq_booster) * AMPLITUDE, -90, 90);
+            }
+            else
+            {
+                RArm.eulerAngles = new Vector3(-40f + Mathf.Sin(Time.time * freq_booster) * AMPLITUDE, 90, -90);
+            }
+        }
+        // BACK TO IDLE POSITION
+        else
+        {
+            transform.rotation = Quaternion.Lerp(transform.rotation, initialRotation, Time.deltaTime);
+            LArm.rotation = Quaternion.Lerp(LArm.rotation, LInitialRotation, Time.deltaTime);
+            RArm.rotation = Quaternion.Lerp(RArm.rotation, RInitialRotation, Time.deltaTime);
         }
 	}
 
-    void BecomeExcited()
+
+
+
+    public void BecomeExcited()
     {
         excited = true;
 
         if (Random.value > 0.5f)
             LArm_excited = true;
-        LArm_excited = false;
-
-        if (LArm_excited)
-            transform.eulerAngles = new Vector3(0, 0, -25);
         else
-            transform.eulerAngles = new Vector3(0, 0, 25);
+            LArm_excited = false;
     }
-    void StopExcitement()
+    public void StopExcitement()
     {
         excited = false;
-        transform.eulerAngles = new Vector3(0, 0, 0);
     }
 
 }
